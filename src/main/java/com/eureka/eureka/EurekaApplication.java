3d8e3f9aebdae34fe31b60f5114d9a0e50397e47
Rabin <rabin.shrestha7@gmail.com>
2023-07-09 12:01:08 -0700

    microservice-eureka

--- src/main/java/com/eureka/eureka/EurekaApplication.java
+++ src/main/java/com/eureka/eureka/EurekaApplication.java
@@ -1,17 +1,16 @@
-package com.client.client;
+package com.eureka.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-//@EnableEurekaClient
-public class ClientApplication {
+@EnableEurekaServer
+public class EurekaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ClientApplication.class, args);
+		SpringApplication.run(EurekaApplication.class, args);
 	}
 
 }
--- src/test/java/com/eureka/eureka/EurekaApplicationTests.java
+++ src/test/java/com/eureka/eureka/EurekaApplicationTests.java
@@ -1,10 +1,10 @@
-package com.client.client;
+package com.eureka.eureka;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ClientApplicationTests {
+class EurekaApplicationTests {
 
 	@Test
 	void contextLoads() {
